1fd1a38115788b806e24b9b1b8d0b29577a6a167
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-21 22:28:39 +0800

    asd
    asdasdadasd

---
+++ arduino/fingerprint2/fingerprint2.ino
@@ -0,0 +1,614 @@
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+#include <WiFi.h>
+#include "DFRobotDFPlayerMini.h"
+#include <Wire.h>
+#include <HTTPClient.h>
+#include <HardwareSerial.h>
+
+
+const char* ssid = "NoName";
+const char* password = "54548484";
+const char* fp_name_in = "testin";
+const char* fp_name_out = "testout";
+// Define software serial pins
+#define RX_PIN 18
+#define TX_PIN 5
+#define TOUCH2 2
+
+#define RXfp1_PIN 16
+#define TXfp1_PIN 17
+#define TOUCH1 4
+
+#define RX2_PIN 12
+#define TX2_PIN 14
+
+#define IR_PIN 34     // ESP32 pin GPIO18 connected to OUT pin of IR obstacle avoidance sensor
+#define RELAY_PIN 13  // ESP32 pin GPIO16, which connects to the solenoid lock via the relay
+
+
+// Create SoftwareSerial object
+HardwareSerial mySerialfp(2);  // Use UART2 (index 2)
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+SoftwareSerial DFPSerial(RX2_PIN, TX2_PIN);
+
+// Initialize the fingerprint sensor
+DFRobotDFPlayerMini player;
+
+Adafruit_Fingerprint finger2 = Adafruit_Fingerprint(&mySerial);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerialfp);
+
+int loginmode = 1;
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(57600);
+  DFPSerial.begin(9600);
+  mySerialfp.begin(57600, SERIAL_8N1, RXfp1_PIN, TXfp1_PIN);
+
+  // Wait for serial to initialize
+  while (!Serial)
+    ;
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+
+  // Initialize the fingerprint sensor
+  if (finger2.verifyPassword()) {
+    Serial.println("Fingerprint sftwr sensor detected!");
+  } else {
+    Serial.println("Fingerprint sftwr sensor not detected!");
+    while (1)
+      ;
+  }
+
+  if (finger.verifyPassword()) {
+    Serial.println("Fingerprint hrdwr detected!");
+  } else {
+    Serial.println("Fingerprint hrdwr sensor not detected!");
+    while (1)
+      ;
+  }
+
+  if (player.begin(DFPSerial)) {
+    Serial.println("OK");
+
+    // Set volume to maximum (0 to 30).
+    player.volume(10);
+    // Play the first MP3 file on the SD card
+    player.play(1);
+  } else {
+    Serial.println("Connecting to DFPlayer Mini failed!");
+  }
+
+  // Serial.println(F("Reading sensor parameters"));
+  // finger.getParameters();
+  // Serial.print(F("Status: 0x"));
+  // Serial.println(finger.status_reg, HEX);
+  // Serial.print(F("Sys ID: 0x"));
+  // Serial.println(finger.system_id, HEX);
+  // Serial.print(F("Capacity: "));
+  // Serial.println(finger.capacity);
+  // Serial.print(F("Security level: "));
+  // Serial.println(finger.security_level);
+  // Serial.print(F("Device address: "));
+  // Serial.println(finger.device_addr, HEX);
+  // Serial.print(F("Packet len: "));
+  // Serial.println(finger.packet_len);
+  // Serial.print(F("Baud rate: "));
+  // Serial.println(finger.baud_rate);
+
+  finger.getTemplateCount();
+
+  if (finger.templateCount == 0) {
+    Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+  } else {
+    Serial.println("Waiting for valid finger...");
+    Serial.print("Sensor contains ");
+    Serial.print(finger.templateCount);
+    Serial.println(" templates");
+  }
+
+
+
+
+  pinMode(IR_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(TOUCH1, INPUT);
+  pinMode(TOUCH2, INPUT);
+}
+
+void loop() {
+
+
+  if (digitalRead(TOUCH1) == HIGH) {
+    Serial.println("TOUCH SENSOR 1");
+  }
+
+
+  // String registermode = getFingerprintmode(fp_name_out);
+
+  // if (registermode == "0") {
+    int test = getFingerprintEnroll(4);
+    if (test) {
+      downloadFingerprintTemplate(4);
+    }
+  // }
+
+
+  // if (loginmode) {
+  //   int id = getFingerprintIDez();
+  //   downloadFingerprintTemplate2(id);
+  // } else {
+  //   downloadFingerprintTemplate(3);
+  // }
+
+
+  int state = digitalRead(IR_PIN);
+
+  if (state == LOW) {
+    Serial.println("The obstacle is present");
+    digitalWrite(RELAY_PIN, HIGH);  // unlock the door
+  } else {
+    Serial.println("The obstacle is NOT present");
+    digitalWrite(RELAY_PIN, LOW);  // unlock the door
+  }
+
+  // digitalWrite(RELAY_PIN, HIGH);  // unlock the door
+  // delay(5000);
+  // digitalWrite(RELAY_PIN, LOW);  // lock the door
+  // delay(5000);
+
+
+  // if object detect for 5 sec continous
+  //  it is a door , lock it
+
+  //  if already login finger but 10 sec no object detct
+  //  alarm
+
+  // if object detect for 5 sec continous but alr login fingerpirnt
+  //  cancel
+
+
+  // alarm silakan amsuk , bila pintu baru buka
+
+  //alarm kalau ada org tapi 1 fingerprint
+
+  //fp x detect ,
+
+
+
+  //register fp , dekat fp out
+
+  //fp settings dekat website
+
+
+
+  //fp transfer data to another fp
+
+  // dobule fp
+  //test touchsense
+
+
+
+
+
+
+  // delay(50);  //don't ned to run this at full speed.
+}
+
+
+uint8_t getFingerprintEnroll(int id) {
+  int p = -1;
+  Serial.print("Waiting for valid finger to enroll as #");
+  Serial.println(id);
+
+  // First sensor enrollment process (unchanged)
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+      case FINGERPRINT_OK:
+        Serial.println("Image taken");
+        break;
+      case FINGERPRINT_NOFINGER:
+        Serial.print(".");
+        break;
+      case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        break;
+      case FINGERPRINT_IMAGEFAIL:
+        Serial.println("Imaging error");
+        break;
+      default:
+        Serial.println("Unknown error");
+        break;
+    }
+  }
+
+  // Convert image and create model
+  p = finger.image2Tz(1);
+  if (p != FINGERPRINT_OK) return p;
+
+  Serial.println("Remove finger");
+  delay(2000);
+
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+
+  Serial.println("Place same finger again");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    if (p == FINGERPRINT_NOFINGER) Serial.print(".");
+  }
+
+  p = finger.image2Tz(2);
+  if (p != FINGERPRINT_OK) return p;
+
+  p = finger.createModel();
+  if (p != FINGERPRINT_OK) return p;
+
+  p = finger.storeModel(id);
+  if (p != FINGERPRINT_OK) return p;
+
+  Serial.println("Stored in sensor 1!");
+
+  // Now enroll into second sensor (finger2)
+  uint8_t buffer[534]; // Buffer to hold model data
+  
+
+
+
+// bool enrollFingerprintTemplate() {
+//   int id = 1;  // ID for the template
+//   Serial.println("Place your finger on the sensor...");
+
+//   if (finger.getImage() != FINGERPRINT_OK) return false;
+
+//   if (finger.createModel() != FINGERPRINT_OK) return false;
+
+//   // Load the model into buffer
+//   if (finger.loadModel(id) != FINGERPRINT_OK) return false;
+
+//   // Send template data to ESP32 serial buffer
+//   if (finger.getModel() == FINGERPRINT_OK) {
+//     Serial.println("Fingerprint template created successfully.");
+
+//     postFingerprintTemplate();
+//     return true;
+//   } else {
+//     Serial.println("Failed to create fingerprint template.");
+//     return false;
+//   }
+// }
+
+
+
+
+void postFingerprintTemplate(uint8_t* fp) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("https://fast.e-veterinar.com/post_fp");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Convert the fingerprint template data to a hex string
+    String hexTemplate = "";
+    for (int i = 0; i < 512; i++) {
+      if (fp[i] < 16) hexTemplate += "0";  // Add leading zero for single hex digits
+      hexTemplate += String(fp[i], HEX);
+    }
+
+
+    // Prepare POST data
+    String postData = "post_fp=" + hexTemplate + "fp=" + hexTemplate;
+
+    Serial.println("posting data  data is :");
+
+    Serial.println(postData);
+
+    // Send POST request
+    int httpResponseCode = http.POST(postData);  // Use .c_str() to convert String to const char*
+
+    // Handle the response
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Server response: " + response);
+    } else {
+      Serial.print("Error in POST request, HTTP code: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi is not connected");
+  }
+}
+
+void postFingerprintID(uint8_t* fp) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("https://fast.e-veterinar.com/login_fp");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Convert the fingerprint template data to a hex string
+    String hexTemplate = "";
+    for (int i = 0; i < 512; i++) {
+      if (fp[i] < 16) hexTemplate += "0";  // Add leading zero for single hex digits
+      hexTemplate += String(fp[i], HEX);
+    }
+
+
+    // Prepare POST data
+    String postData = "login_fp=" + hexTemplate + "fp=" + hexTemplate;
+
+    Serial.println("posting data  data is :");
+
+    Serial.println(postData);
+
+    // Send POST request
+    int httpResponseCode = http.POST(postData);  // Use .c_str() to convert String to const char*
+
+    // Handle the response
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Server response: " + response);
+    } else {
+      Serial.print("Error in POST request, HTTP code: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi is not connected");
+  }
+}
+
+
+
+void printHex(int num, int precision) {
+  char tmp[16];
+  char format[128];
+
+  sprintf(format, "%%.%dX", precision);
+
+  sprintf(tmp, format, num);
+  Serial.print(tmp);
+}
+
+
+uint8_t downloadFingerprintTemplate(uint16_t id) {
+  Serial.println("------------------------------------");
+  Serial.print("Attempting to load #");
+  Serial.println(id);
+  uint8_t p = finger.loadModel(id);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template ");
+      Serial.print(id);
+      Serial.println(" loaded");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    default:
+      Serial.print("Unknown error ");
+      Serial.println(p);
+      return p;
+  }
+
+  // OK success!
+
+  Serial.print("Attempting to get #");
+  Serial.println(id);
+  p = finger.getModel();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template ");
+      Serial.print(id);
+      Serial.println(" transferring:");
+      break;
+    default:
+      Serial.print("Unknown error ");
+      Serial.println(p);
+      return p;
+  }
+
+  // one data packet is 267 bytes. in one data packet, 11 bytes are 'useless' :D
+  uint8_t bytesReceived[534];  // 2 data packets
+  memset(bytesReceived, 0xff, 534);
+
+  uint32_t starttime = millis();
+  int i = 0;
+  while (i < 534 && (millis() - starttime) < 20000) {
+    if (mySerial.available()) {
+      bytesReceived[i++] = mySerial.read();
+    }
+  }
+  Serial.print(i);
+  Serial.println(" bytes read.");
+  Serial.println("Decoding packet...");
+
+  uint8_t fingerTemplate[512];  // the real template
+  memset(fingerTemplate, 0xff, 512);
+
+  // filtering only the data packets
+  int uindx = 9, index = 0;
+  memcpy(fingerTemplate + index, bytesReceived + uindx, 256);  // first 256 bytes
+  uindx += 256;                                                // skip data
+  uindx += 2;                                                  // skip checksum
+  uindx += 9;                                                  // skip next header
+  index += 256;                                                // advance pointer
+  memcpy(fingerTemplate + index, bytesReceived + uindx, 256);  // second 256 bytes
+
+  for (int i = 0; i < 512; ++i) {
+    //Serial.print("0x");
+    printHex(fingerTemplate[i], 2);
+    //Serial.print(", ");
+  }
+
+  // Pass the entire fingerTemplate array (not just fingerTemplate[512])
+  // postFingerprintTemplate(fingerTemplate);
+
+  Serial.println("\ndone.");
+  delay(10000);  // Delay to prevent repeated posting
+
+  return p;
+}
+
+
+uint8_t downloadFingerprintTemplate2(uint16_t id) {
+  Serial.println("------------------------------------");
+  Serial.print("Attempting to load #");
+  Serial.println(id);
+  uint8_t p = finger.loadModel(id);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template ");
+      Serial.print(id);
+      Serial.println(" loaded");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    default:
+      Serial.print("Unknown error ");
+      Serial.println(p);
+      return p;
+  }
+
+  // OK success!
+
+  Serial.print("Attempting to get #");
+  Serial.println(id);
+  p = finger.getModel();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template ");
+      Serial.print(id);
+      Serial.println(" transferring:");
+      break;
+    default:
+      Serial.print("Unknown error ");
+      Serial.println(p);
+      return p;
+  }
+
+  // one data packet is 267 bytes. in one data packet, 11 bytes are 'useless' :D
+  uint8_t bytesReceived[534];  // 2 data packets
+  memset(bytesReceived, 0xff, 534);
+
+  uint32_t starttime = millis();
+  int i = 0;
+  while (i < 534 && (millis() - starttime) < 20000) {
+    if (mySerial.available()) {
+      bytesReceived[i++] = mySerial.read();
+    }
+  }
+  Serial.print(i);
+  Serial.println(" bytes read.");
+  Serial.println("Decoding packet...");
+
+  uint8_t fingerTemplate[512];  // the real template
+  memset(fingerTemplate, 0xff, 512);
+
+  // filtering only the data packets
+  int uindx = 9, index = 0;
+  memcpy(fingerTemplate + index, bytesReceived + uindx, 256);  // first 256 bytes
+  uindx += 256;                                                // skip data
+  uindx += 2;                                                  // skip checksum
+  uindx += 9;                                                  // skip next header
+  index += 256;                                                // advance pointer
+  memcpy(fingerTemplate + index, bytesReceived + uindx, 256);  // second 256 bytes
+
+
+  // Pass the entire fingerTemplate array (not just fingerTemplate[512])
+  postFingerprintID(fingerTemplate);
+
+  Serial.println("\ndone.");
+  delay(10000);  // Delay to prevent repeated posting
+
+  return p;
+}
+
+
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK) return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK) return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK) return -1;
+
+  // found a match!
+  Serial.print("Found ID #");
+  Serial.print(finger.fingerID);
+  Serial.print(" with confidence of ");
+  Serial.println(finger.confidence);
+  return finger.fingerID;
+}
+
+
+
+void uploadFingerprintToSensor(Adafruit_Fingerprint& sourceSensor, Adafruit_Fingerprint& targetSensor, int id) {
+  uint8_t p = sourceSensor.loadModel(id);  // Load model from source sensor (id = 1)
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Fingerprint model loaded from Sensor 1");
+
+    // Now upload the model to the second sensor
+    p = targetSensor.storeModel(id);
+    if (p == FINGERPRINT_OK) {
+      Serial.println("Fingerprint model uploaded to Sensor 2");
+    } else {
+      Serial.println("Failed to upload fingerprint model to Sensor 2");
+    }
+  } else {
+    Serial.println("Failed to load fingerprint model from Sensor 1");
+  }
+}
+
+
+String getFingerprintmode(String fp_name) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("https://fast.e-veterinar.com/fp_mode");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Convert the fingerprint template data to a hex string
+
+
+    // Prepare POST data
+    String postData = "fp_mode=" + fp_name + "fp_name=" + fp_name;
+
+    // Serial.println("posting data  data is :");
+
+    // Serial.println(postData);
+
+    // Send POST request
+    int httpResponseCode = http.POST(postData);  // Use .c_str() to convert String to const char*
+
+    // Handle the response
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("response  data is :");
+
+      Serial.println(response);
+      Serial.println("thats all");
+      return response;
+    } else {
+      Serial.print("Error in POST request, HTTP code: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi is not connected");
+  }
+}
