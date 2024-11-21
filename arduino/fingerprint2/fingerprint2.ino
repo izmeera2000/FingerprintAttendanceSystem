63c07447c62c037d1a9b442028283b05730766ea
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-22 03:50:57 +0800

    asds
    adasdsad

--- arduino/fingerprint2/fingerprint2.ino
+++ arduino/fingerprint2/fingerprint2.ino
@@ -2,8 +2,8 @@
 #include "DFRobotDFPlayerMini.h"
 
 // Use pins 2 and 3 to communicate with DFPlayer Mini
-static const uint8_t PIN_MP3_TX = 26; // Connects to module's RX 
-static const uint8_t PIN_MP3_RX = 27; // Connects to module's TX 
+static const uint8_t PIN_MP3_TX = 26;  // Connects to module's RX
+static const uint8_t PIN_MP3_RX = 27;  // Connects to module's TX
 SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
 
 // Create the Player object
@@ -17,16 +17,14 @@ void setup() {
   softwareSerial.begin(9600);
 
   // Start communication with DFPlayer Mini
-if (player.begin(softwareSerial, true, false)) {  // Enable debug prints
-  Serial.println("Connected to DFPlayer Mini successfully!");
-  player.volume(20);
-  player.play(1);
-} else {
-  Serial.println("Failed to initialize DFPlayer Mini.");
+  if (player.begin(softwareSerial, true, false)) {  // Enable debug prints
+    Serial.println("Connected to DFPlayer Mini successfully!");
+    player.volume(20);
+    player.play(1);
+  } else {
+    Serial.println("Failed to initialize DFPlayer Mini.");
+  }
 }
-)
 
 void loop() {
-
- 
-   }
+}
