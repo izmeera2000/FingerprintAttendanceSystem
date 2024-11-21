456b2d99e6bb57842b40e900d91f75542c47032d
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-22 02:06:01 +0800

    asdas
    dassdaasdsa

--- arduino/fingerprint/fingerprint.ino
+++ arduino/fingerprint/fingerprint.ino
@@ -50,12 +50,12 @@ void setup() {
   while (!Serial)
     ;
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
+  // Serial.println("Connected to WiFi");
 
 
   // Initialize the fingerprint sensor
