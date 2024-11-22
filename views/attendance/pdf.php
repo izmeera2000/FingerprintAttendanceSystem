5344e1361fc9a74d395598e4e7c3de4d1720a74a
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-22 15:28:31 +0800

    asd
    asdsadasdasdas

--- views/attendance/pdf.php
+++ views/attendance/pdf.php
@@ -8,7 +8,7 @@
     <h1>Test Form</h1>
     <form action="https://fast.e-veterinar.com/fp_mode" method="post">
         <label for="name">Name:</label>
-        <input type="text" id="name" name="fp_name" required><br><br>
+        <input type="text" id="fp_name" name="fp_name" required><br><br>
         <!-- <label for="email">fp:</label>
         <input type="email" id="fp" name="fp" required><br><br> -->
         <input type="submit" value="Submit" name="fp_mode">
