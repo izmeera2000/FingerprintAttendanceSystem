11478592d855df1e0c85b0fff945521841f62caf
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-27 08:02:12 +0800

    asd
    asdasdas

--- admin/server.php
+++ admin/server.php
@@ -2553,7 +2553,6 @@ ORDER BY user_id, tarikh;";
 
   while ($attslot = mysqli_fetch_assoc($resultsa)) {
     if ($attslot['count'] >= 7) {
-      echo $attslot['user_id'] . "\n";
 
       $id = $attslot['user_id'];
 
@@ -2570,123 +2569,124 @@ ORDER BY user_id, tarikh;";
             WHERE a.id = '$id'";
       $results2 = mysqli_query($db, $query2);
       while ($row = $results2->fetch_assoc()) {
+        $id2 = $row['id'];
         $nama = $row['nama'];
         $kp = $row['kp'];
         $kursus = $row['kursus'];
         $ndp = $row['ndp'];
         $sem_start = $row['sem_start'];
         $email = $row['email'];
+echo $email;
 
-      }
-      $sem = getSemesterByNumber($sem_start);
+        $sem = getSemesterByNumber($sem_start);
 
-      $pdf = new exFPDF();
-      // add a page
-      $pdf->SetFont('arial', '', 12);
+        $pdf = new exFPDF();
+        // add a page
+        $pdf->SetFont('arial', '', 12);
 
-      $pdf->AddPage();
-      // set the source file
-      $pdf->setSourceFile("assets/pdf/jtp2.pdf");
+        $pdf->AddPage();
+        // set the source file
+        $pdf->setSourceFile("assets/pdf/jtp2.pdf");
 
-      // import page 1
-      $tplId = $pdf->importPage(1);
-      // use the imported page and place it at point 10,10 with a width of 100 mm
-      $pdf->useTemplate($tplId, ['adjustPageSize' => true]);
-      $pdf->SetXY(67, 102.6);
-      $pdf->Write(0, $nama);
+        // import page 1
+        $tplId = $pdf->importPage(1);
+        // use the imported page and place it at point 10,10 with a width of 100 mm
+        $pdf->useTemplate($tplId, ['adjustPageSize' => true]);
+        $pdf->SetXY(67, 102.6);
+        $pdf->Write(0, $nama);
 
-      $pdf->SetXY(67, 107.6);
-      $pdf->Write(0, $kp);
+        $pdf->SetXY(67, 107.6);
+        $pdf->Write(0, $kp);
 
-      $pdf->SetXY(67, 112.6);
-      $pdf->Write(0, $ndp);
+        $pdf->SetXY(67, 112.6);
+        $pdf->Write(0, $ndp);
 
-      $pdf->SetXY(67, 117.6);
-      $pdf->Write(0, $kursus);
+        $pdf->SetXY(67, 117.6);
+        $pdf->Write(0, $kursus);
 
-      $pdf->SetXY(67, 122.6);
-      $pdf->Write(0, $sem);
+        $pdf->SetXY(67, 122.6);
+        $pdf->Write(0, $sem);
 
-      $pdf->AddPage();                // Create a new page in the output PDF
-      // // import page 1
-      $tplId = $pdf->importPage(2);
-      // use the imported page and place it at point 10,10 with a width of 100 mm
-      $pdf->useTemplate($tplId, ['adjustPageSize' => true]);
-
-
-
-      $pdf->SetXY(67, 44.2);
-      $pdf->Write(0, $nama);
-
-      $pdf->SetXY(67, 49.1);
-      $pdf->Write(0, $kp);
-
-      $pdf->SetXY(67, 54);
-      $pdf->Write(0, $ndp);
-
-      $pdf->SetXY(67, 58.9);
-      $pdf->Write(0, $kursus);
-
-      $pdf->SetXY(67, 63.8);
-      $pdf->Write(0, $sem);
-      $pdf->Ln(10);
+        $pdf->AddPage();                // Create a new page in the output PDF
+        // // import page 1
+        $tplId = $pdf->importPage(2);
+        // use the imported page and place it at point 10,10 with a width of 100 mm
+        $pdf->useTemplate($tplId, ['adjustPageSize' => true]);
 
 
-      $table = new easyTable($pdf, '%{10,25, 30, 25,10}', 'align:C; border:1 ;border-width:0.1;');
-      $table->easyCell('', 'border:0');
-      $table->easyCell('TARIKH', '  bgcolor:#d9d9d9; align:C;font-style:B;border-width:0.1;');
-      $table->easyCell('KURSUS', '  bgcolor:#d9d9d9;align:C;font-style:B;border-width:0.1;');
-      $table->easyCell('SLOT', '  bgcolor:#d9d9d9;align:C;font-style:B;border-width:0.1;');
-      $table->easyCell('', 'border:0');
 
-      $table->printRow();
+        $pdf->SetXY(67, 44.2);
+        $pdf->Write(0, $nama);
+
+        $pdf->SetXY(67, 49.1);
+        $pdf->Write(0, $kp);
+
+        $pdf->SetXY(67, 54);
+        $pdf->Write(0, $ndp);
+
+        $pdf->SetXY(67, 58.9);
+        $pdf->Write(0, $kursus);
+
+        $pdf->SetXY(67, 63.8);
+        $pdf->Write(0, $sem);
+        $pdf->Ln(10);
 
 
-      $query = "SELECT  DATE_FORMAT(tarikh, '%d/%m/%Y') AS tarikh_f, COUNT(*) as count  
+        $table = new easyTable($pdf, '%{10,25, 30, 25,10}', 'align:C; border:1 ;border-width:0.1;');
+        $table->easyCell('', 'border:0');
+        $table->easyCell('TARIKH', '  bgcolor:#d9d9d9; align:C;font-style:B;border-width:0.1;');
+        $table->easyCell('KURSUS', '  bgcolor:#d9d9d9;align:C;font-style:B;border-width:0.1;');
+        $table->easyCell('SLOT', '  bgcolor:#d9d9d9;align:C;font-style:B;border-width:0.1;');
+        $table->easyCell('', 'border:0');
+
+        $table->printRow();
+
+
+        $query = "SELECT  DATE_FORMAT(tarikh, '%d/%m/%Y') AS tarikh_f, COUNT(*) as count  
                 FROM `attendance_slot` 
                 WHERE user_id = '$id' 
                   AND tarikh BETWEEN '$start_date' AND '$end_date';";
-      $results = mysqli_query($db, $query);
-      $total = 0;
-      while ($attslot = mysqli_fetch_assoc($results)) {
+        $results = mysqli_query($db, $query);
+        $total = 0;
+        while ($attslot = mysqli_fetch_assoc($results)) {
+
+          $table->easyCell('', 'border:0');
+          $table->easyCell($attslot['tarikh_f'], 'align:C;border-width:0.1;');
+          $table->easyCell('-', 'align:C;border-width:0.1;');
+          $table->easyCell($attslot['count'], 'align:C;border-width:0.1; ');
+          $table->easyCell('', 'border:0');
+          $table->printRow();
+          $total = $total + $attslot['count'];
+        }
+
+
+
 
         $table->easyCell('', 'border:0');
-        $table->easyCell($attslot['tarikh_f'], 'align:C;border-width:0.1;');
-        $table->easyCell('-', 'align:C;border-width:0.1;');
-        $table->easyCell($attslot['count'], 'align:C;border-width:0.1; ');
+        $table->easyCell(' JUMLAH SLOT', '   bgcolor:#d9d9d9; align:L;font-style:B;border:LTB;border-width:0.1;');
+        $table->easyCell(' ', ' bgcolor:#d9d9d9; border:BT;border-width:0.1;');
+        $table->easyCell('22', '  bgcolor:#d9d9d9;align:C;font-style:B;border-width:0.1;');
         $table->easyCell('', 'border:0');
         $table->printRow();
-        $total = $total + $attslot['count'];
+
+        $table->endTable(5);
+
+
+
+
+        $pdf->Output('F', 'jtp2.pdf');
+        $filePath = __DIR__ . '/../jtp2.pdf';
+
+        $var = array(
+          // 'link' => $site_url . "kaunseling/temujanji/$event_id", // Example variable
+          'attachment' => $filePath,
+          'attachment_name' => 'jtp2.pdf',
+          'alasan' => "test", // Example variable
+        );
+        // echo "test2";
+        sendmail($email, "Aduan Disiplin Pelajar", 'jtp2.php', $var);
+        // unlink('jtp2.pdf'); // Removes the file after sending the email
       }
-
-
-
-
-      $table->easyCell('', 'border:0');
-      $table->easyCell(' JUMLAH SLOT', '   bgcolor:#d9d9d9; align:L;font-style:B;border:LTB;border-width:0.1;');
-      $table->easyCell(' ', ' bgcolor:#d9d9d9; border:BT;border-width:0.1;');
-      $table->easyCell('22', '  bgcolor:#d9d9d9;align:C;font-style:B;border-width:0.1;');
-      $table->easyCell('', 'border:0');
-      $table->printRow();
-
-      $table->endTable(5);
-
-
-
-
-      $pdf->Output('F', 'jtp2.pdf');
-      $filePath = __DIR__ . '/../jtp2.pdf';
-
-      $var = array(
-        // 'link' => $site_url . "kaunseling/temujanji/$event_id", // Example variable
-        'attachment' => $filePath,
-        'attachment_name' => 'jtp2.pdf',
-        'alasan' => "test", // Example variable
-      );
-      // echo "test2";
-      sendmail($email, "Aduan Disiplin Pelajar", 'jtp2.php', $var);
-      unlink('jtp2.pdf'); // Removes the file after sending the email
-
     }
 
   }
