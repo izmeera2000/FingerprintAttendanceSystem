ecf497b103b08ae2be22c6e526fa86ced5707071
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-12-09 01:42:02 +0800

    asds
    adasdasdsa

--- arduino/fingerprint/fingerprint.ino
+++ arduino/fingerprint/fingerprint.ino
@@ -186,7 +186,7 @@ void loop() {
   if (player.available()) {
     printDetail(player.readType(), player.read()); //Print the detail message from DFPlayer to handle different errors and states.
   }
-}
+  // delay(1000);
 
   // if (test == "login") {
 
@@ -204,7 +204,7 @@ void loop() {
 
   //   registerFP();
   // }
-  delay(1000);  // Small delay to debounce (adjust as needed)
+  // delay(1000);  // Small delay to debounce (adjust as needed)
 
 
 
@@ -238,12 +238,65 @@ void loop() {
   // dobule fp
   //test touchsense
 
-
-
-
-
-
   // delay(50);  //don't ned to run this at full speed.
 }
-void printDetail(uint8_t type, int value){
+void printDetail(uint8_t type, int value) {
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 }
